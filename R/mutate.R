78564285b768a5268603dccfd6d4bab62989a7a6
hoxo-m <hoxo.smile@gmail.com>
2015-07-01 20:33:26 +0900

    add mutate()

---
+++ R/mutate.R
@@ -0,0 +1,18 @@
+#'
+#' @export
+mutate <- function(.data, ..., .dots = lazyeval::lazy_dots(...)) {
+  dfname <- as.character(substitute(.data))
+  columns <- SparkR::columns(.data)
+  if(length(.dots) == 0) return(SparkR::mutate(.data))
+  
+  input <- to_spark_input(.dots[[1]], dfname, columns)
+  colName <- names(.dots)[1]
+  .dummy_data_frame <- SparkR::withColumn(.data, colName = colName, col = input)
+  if(length(.dots) == 1) {
+    .dummy_data_frame
+  } else {
+    dots <- tail(.dots, -1)
+    assign(".dummy_data_frame", .dummy_data_frame, envir = dots[[1]]$env)
+    mutate(.dummy_data_frame, .dots=dots)
+  }
+}
