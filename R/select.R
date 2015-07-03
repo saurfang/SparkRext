f567d647b7ac47dd8140e526f8ad4975c752af0c
hoxo-m <hoxo.smile@gmail.com>
2015-07-03 21:03:02 +0900

    modify for character input

--- R/select.R
+++ R/select.R
@@ -1,7 +1,14 @@
 #' @export
 select <- function(.data, ..., .dots = lazyeval::lazy_dots(...)) {
-  columns <- SparkR::columns(.data)
-  vars <- dplyr::select_vars_(columns, .dots)
-  cols <- as.list(vars)
+  if(is.character(.dots)) {
+    cols <- as.list(.dots)
+  } else {
+    cols <- Map(function(lazy) lazy$expr, .dots)
+    if(!is.character(unlist(cols))) {
+      columns <- SparkR::columns(.data)
+      vars <- dplyr::select_vars_(columns, .dots)
+      cols <- as.list(vars)
+    }
+  }
   SparkR::select(.data, cols)
 }
